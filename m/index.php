64a2f577f7fe64ec804369767aa31a80d73d1b49
jairolima <msgjairo@gmail.com>
2020-03-01 23:09:16 -0300

    first commit

---
+++ m/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!-- Compiled and minified CSS -->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+     <!-- MEU CSS -->
+     <link rel="stylesheet" href="style.css">
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+
+
+    </head>
+    <body>
+
+
+
+
+<div class="row">
+<div class="col s6 offset-s3 center logo-spc">
+
+<img class="responsive-img" src="img/mbemestar.png" />
+
+
+
+
+</div>
+</div>
+
+
+<div class="row">
+<div class="col s10 offset-s1 center no-padding"><a class="white-text" href="tel:8332282312"><h4>(83) 3228-2312<i class="fas fa-phone fa-sm white-text iconspace"></i></h4></a></div>
+<div class="col s10 offset-s1 center no-padding"><a class="white-text" href="https://api.whatsapp.com/send?phone=5583986180305&text=Ol%C3%A1,%20Policl%C3%ADnica%20BEMESTAR!"><h4>(83) 98618-0305<i class="fab fa-whatsapp fa-sm white-text iconspace"></i></h4></a></div>
+</div>
+        
+
+
+
+<h5 class="center white-text">Site em construção</h5>
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    </body>
+  </html>
